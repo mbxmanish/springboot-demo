b957e0f05f9a7faf06686305c43bced8415dee00
mbxmanish <128045915+mbxmanish@users.noreply.github.com>
2023-04-12 20:13:32 +0100

    Initial Commit

---
+++ employee/src/main/java/com/example/employee/model/Employee.java
@@ -0,0 +1,34 @@
+package com.example.employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="emp_id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email_id")
+    private String emailId;
+}
---
+++ employee/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee (emp_id,first_name,last_name,email_id) VALUES (1,'First','Dev','dev1@test.com');
+INSERT INTO employee (emp_id,first_name,last_name,email_id) VALUES (2,'Second','Dev','dev2@test.com');
+INSERT INTO employee (emp_id,first_name,last_name,email_id) VALUES (3,'Third','Dev','dev3@test.com');
